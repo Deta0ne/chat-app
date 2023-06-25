815675995cd8614f0d6857591d79c480b7faa9b3
Mert <cyhaeed.mert@gmail.com>
2023-06-25 16:35:56 +0300

    Auth control

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './contexts/AuthContext';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import './style.scss';
 
 function App() {
+    const { girisKullanici } = useContext(AuthContext);
+
+    const YönlendirmeKontrol = ({ children }) => {
+        if (!girisKullanici) {
+            return <Navigate to="/login" />;
+        }
+        return children;
+    };
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/">
-                    <Route index element={<Home />} />
+                    <Route
+                        index
+                        element={
+                            <YönlendirmeKontrol>
+                                <Home />
+                            </YönlendirmeKontrol>
+                        }
+                    />
                     <Route path="login" element={<Login />} />
                     <Route path="register" element={<Register />} />
                 </Route>
